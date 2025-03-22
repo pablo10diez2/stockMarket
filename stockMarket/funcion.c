f09c11433e99e8f85b5963892813537bb320513b
pablo10diez2 <pablo.diez@opendeusto.es>
2025-03-22 18:39:41 +0100

    Funciones input básicas

--- stockMarket/funcion.c
+++ stockMarket/funcion.c
@@ -1,7 +1,47 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 #include "funcion.h"
-void iniciarPrograma(){
-    printf("Welcome to the Admin Menu \n");
-    printf("Enter your e-mail: \n");
-    printf("Enter your password: \n");
+
+void gestionCSV(){
+    printf("hola");
 }
+
+bool comprobarCredenciales(){
+    return true;
+}
+
+void iniciarPrograma(){
+    char str[30];
+
+    printf("Welcome to the Admin Menu \n");
+    
+    iniciarSesion();
+    
+}
+
+void iniciarSesion(){
+    char mail[30];
+    char pass[30];
+    int num;
+
+    printf("Introduce tu e-mail: \n");
+    scanf("%s", mail);
+
+    printf("Introduce tu contraseña: \n");
+    scanf("%s", pass);
+
+    if(comprobarCredenciales()){
+        printf("Introduce 1 para gestionar el fichero");
+        scanf("%d", &num);
+        
+        if(num == 1){
+            gestionCSV();
+        }
+    }
+    else{
+        iniciarSesion();
+    }
+}
+
+
--- stockMarket/funcion.h
+++ stockMarket/funcion.h
@@ -1,6 +1,11 @@
 #ifndef FUNCION_H
 #define	FUNCION_H
 
+#include <stdbool.h>
+
+bool comprobarCredenciales();
+void gestionCSV();
+void iniciarSesion();
 void iniciarPrograma();
 
 #endif
