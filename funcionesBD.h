8c5d77d72f89da5d4a7c7c5575e83da45bf0685d
Andoni Arrabal <andoni.arrabal@opendeusto.es>
2025-03-26 17:03:42 +0100

    funciones bd iker

--- funcionesBD.h
+++ funcionesBD.h
@@ -7,5 +7,7 @@
 
 void insertar_nombre(const char *nombre, int id);
 void insertar_apellido(const char *apellido, int id);
+void consultarUsuario(const char *email);
+void actualizarContrasena(const char *email, const char *nuevaContrasena);
 
 #endif
